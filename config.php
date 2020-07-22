f224cbddfca36af07c8a3cbb5a92e7c344a901db
sanook123 <68609618+sanook123@users.noreply.github.com>
2020-07-22 03:16:21 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCVkRcSiF0KmLCwmoo4edtw0vnSykCvFcc');
+define('APIKEY', 'AIzaSyAeIUNhn96qnQ6fnbMZ7GnJf25vWuypBGw');
 define('GJ_CODE', 'US');
-define('SITE_NAME', 'y2b');
-define('TITLENAME', 'y2b');
+define('SITE_NAME', 'you');
+define('TITLENAME', 'you');
 define('EN2DEKEY', '32123wwerwer323tgfdgds');
-define('EMAIL', '1334347212@qq.com');
+define('EMAIL', 'liwen123@yandex.com');
 define('NAME', 'admin');
-define('PASSWORD', 'admin');
+define('PASSWORD', '8431756');
 define('LOGINHTML', '<!DOCTYPE html>
 <html lang="en">
 <head>
